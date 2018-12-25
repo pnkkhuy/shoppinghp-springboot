c919890de3cd51f2a502f809594453536bf01eef
khanhhuy215 <khanhhuy215@gmail.com>
2018-12-25 20:49:08 +0700

    Update shopping

--- shoppinghp/src/main/java/com/shoppinghp/controller/HomeController.java
+++ shoppinghp/src/main/java/com/shoppinghp/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.shoppinghp.controller;
 
+import com.shoppinghp.service.ICategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ICategoryService categoryService;
+
+
 
     @RequestMapping("/")
     public String welcome() {
@@ -13,7 +24,8 @@ public class HomeController {
     }
 
     @RequestMapping("/product")
-    public String product() {
+    public String product(Model model) {
+        model.addAttribute("category_list", categoryService.getAllCategory());
         return "shop/product";
     }
 
